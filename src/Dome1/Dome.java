f1d2abbca063cd11586f9da8e4a81b257c241e4f
luokefeng <357293449@qq.com>
2018-01-02 17:06:54 +0800

    哈哈哈哈哈哈或哈哈哈哈哈哈或哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈

---
+++ src/Dome1/Dome.java
@@ -0,0 +1,24 @@
+package Dome1;
+
+import java.util.Scanner;
+
+public class Dome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       System.out.println("込込込");
+       System.out.println("込込込");	
+       System.out.println("込込込");	
+       System.out.println("込込込");	
+       System.out.println("込込込");	
+       System.out.println("込込込");	
+       System.out.println("込込込");	
+	   int a=nexInt();
+	System.out.println("nexInt");
+	}
+    public static int nexInt(){
+	return 0;
+	
+}
+}
